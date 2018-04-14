849a5d4013c8e25e04585b373611e9c6c09e3c29
Novikov Michael <nmss@tut.by>
2018-04-14 12:04:34 +0300

    terminator

---
+++ ExcelExamle/Terminator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMFinder
+{
+    class Terminator
+    {
+    string hello  = "Hello baby";
+    }
+}
